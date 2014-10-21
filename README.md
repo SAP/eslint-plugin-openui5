d065ae7e2f26fefd9a007c2d46baaa34008780e7
Christoph Kraemer <chr.kraemer@sap.com>
2014-10-21 10:27:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+![OpenUI5](http://openui5.org/images/OpenUI5_new_big_side.png)
+
+[![Build Status](http://img.shields.io/travis/SAP/eslint-plugin-openui5.svg?style=flat)](https://travis-ci.org/SAP/eslint-plugin-openui5)
+[![NPM Version](http://img.shields.io/npm/v/eslint-plugin-openui5.svg?style=flat)](https://www.npmjs.org/package/eslint-plugin-openui5)
+
+eslint-plugin-openui5
+=====================
+
+This repository contains rules that are used by OpenUI5 to check the sources.
+
+Checks
+------
+
+line-endings: Check if a file has the correct line endings
+
+browserify
+----------
+Before installing the maven artifact, you need to build the browserify version of the plugin. This can then be used
+by the eslint-maven-plugin.
+
+License
+-------
+
+(c) Copyright 2014 SAP SE or an SAP affiliate company
+Licensed under the Apache License, Version 2.0 - see LICENSE.
---
+++ test/rules/line-endings.js
@@ -0,0 +1,53 @@
+// Copyright 2014 SAP SE.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http: //www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+/**
+ * @fileoverview Check for correct line endings
+ * @author Christoph Kraemer <chr.kraemer@sap.com>
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var linter = require("eslint").linter,
+    ESLintTester = require("eslint-tester"),
+    eslintTester = new ESLintTester(linter);
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+eslintTester.addRuleTest("lib/rules/line-endings", {
+
+    valid: [
+        "var a = 123;\n"
+    ],
+
+    invalid: [
+        {
+            code: "var a = 123;\r",
+            errors: [
+                { message: "Invalid line endings.", type: "Program" }
+            ]
+        },
+        {
+            code: "var a = 123;\r\n",
+            errors: [
+                { message: "Invalid line endings.", type: "Program" }
+            ]
+        }
+    ]
+});

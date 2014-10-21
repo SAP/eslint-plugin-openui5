b32b77133ddb1b8bca7f4a6e40ff09423d5c4c69
Christoph Kraemer <chr.kraemer@sap.com>
2014-10-21 10:38:30 +0200

    Install ESLint before travis build

--- test/rules/line-endings.js
+++ test/rules/line-endings.js
@@ -23,8 +23,8 @@
 //------------------------------------------------------------------------------
 
 var linter = require("eslint").linter,
-    ESLintTester = require("eslint-tester"),
-    eslintTester = new ESLintTester(linter);
+	ESLintTester = require("eslint-tester"),
+	eslintTester = new ESLintTester(linter);
 
 //------------------------------------------------------------------------------
 // Tests
@@ -32,22 +32,22 @@ var linter = require("eslint").linter,
 
 eslintTester.addRuleTest("lib/rules/line-endings", {
 
-    valid: [
-        "var a = 123;\n"
-    ],
+	valid: [
+		"var a = 123;\n"
+	],
 
-    invalid: [
-        {
-            code: "var a = 123;\r",
-            errors: [
-                { message: "Invalid line endings.", type: "Program" }
-            ]
-        },
-        {
-            code: "var a = 123;\r\n",
-            errors: [
-                { message: "Invalid line endings.", type: "Program" }
-            ]
-        }
-    ]
+	invalid: [
+		{
+			code: "var a = 123;\r",
+			errors: [
+				{ message: "Invalid line endings.", type: "Program" }
+			]
+		},
+		{
+			code: "var a = 123;\r\n",
+			errors: [
+				{ message: "Invalid line endings.", type: "Program" }
+			]
+		}
+	]
 });
